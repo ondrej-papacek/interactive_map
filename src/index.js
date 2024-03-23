728145767d10e0507d8eda051a195492c49b4635
Ondrej Papacek <ondrejpapacek@gmail.com>
2024-03-23 20:11:50 +0100

    map - images for layers 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+let map = L.map('mapid').setView([0, 0], 2);
+let baseLayer = L.imageOverlay('./images/Eveeye-Regions-2.png', [[0, 0], [5090, 5300]]);
