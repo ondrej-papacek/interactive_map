02c03a66b5da24772bf4cb7ed00a0ccc55675c9e
Ondrej Papacek <ondrejpapacek@gmail.com>
2024-03-23 20:16:58 +0100

    map - renaming

--- src/index.js
+++
@@ -1,2 +0,0 @@
-let map = L.map('mapid').setView([0, 0], 2);
-let baseLayer = L.imageOverlay('./images/Eveeye-Regions-2.png', [[0, 0], [5090, 5300]]);
---
+++ src/script.js
@@ -0,0 +1,2 @@
+var map = L.map('mapid').setView([0, 0], 2);
+var baseLayer = L.imageOverlay('./images/', [[0, 0], [5090, 5300]]);
